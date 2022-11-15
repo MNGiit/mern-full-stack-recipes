d9ff4e94de4ed9b66720dcb918c7b1a1f3df2674
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-11-14 23:51:23 -0500

    component to log user out

---
+++ frontend/src/components/LogOutButton.jsx
@@ -0,0 +1,16 @@
+// import {logOut} from '../../utilities/users-service';
+import {logOut} from '../utilities/users-service';
+
+export default function UserLogOut({user, setUser}) {
+    function handleLogOut() {
+        logOut();
+        setUser(null);
+    }
+
+    return (
+        <div>
+            <div>{user.newUser.name}</div>
+            <button onClick={handleLogOut}>Log out</button>
+        </div>
+    );
+}
